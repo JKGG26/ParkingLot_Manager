7f9240ef5108e33a0d8e5f295a3f352a5a6df826
JhormanGonzalez <jgonzalez202303@gmail.com>
2024-07-19 11:25:54 -0500

    http utils and get post params function added

---
+++ RESTAPI/Apps/ParkingApp/utils/http_utils.py
@@ -0,0 +1,13 @@
+
+
+def get_post_params(request, params_required) -> tuple:
+    params_gotten = {}
+    for param in params_required:
+        try:
+            params_gotten[param] = request.POST.get(param)
+            if params_gotten[param] is None:
+                raise ValueError()
+        except:
+            return {}, f"Param '{param}' required"
+    
+    return params_gotten, None
